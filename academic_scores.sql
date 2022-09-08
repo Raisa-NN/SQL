3f9e3f3c78f30996c861627657d554e271e96a4c
Raisa-NN <60449775+Raisa-NN@users.noreply.github.com>
2022-09-08 17:24:30 -0400

    Added project files to repo

---
+++ academic_scores.sql
@@ -0,0 +1,35 @@
+-- TABLE
+CREATE TABLE 'county_info' ('id' INTEGER,'unemp' REAL,'wage' REAL,'distance' REAL,'region' TEXT,'avg_county_tuition' REAL);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE reports_student_colleges(
+  id INT,
+  academic_score REAL,
+  student_tuition INT,
+  education INT,
+  unemp REAL,
+  wage REAL,
+  distance REAL,
+  region TEXT,
+  avg_county_tuition REAL,
+  fcollege TEXT,
+  mcollege TEXT,
+  home TEXT,
+  urban TEXT,
+  income TEXT,
+  gender TEXT,
+  DOB TEXT,
+  ethnicity TEXT,
+  academic_info_id INT,
+  county_id INT,
+  family_details_id INT
+);
+CREATE TABLE 'student_academic_info' ('id' INTEGER,'academic_score' REAL,'student_tuition' INTEGER,'education' INTEGER);
+CREATE TABLE 'student_family_details' ('id' INTEGER,'fcollege' TEXT,'mcollege' TEXT,'home' TEXT,'urban' TEXT,'income' TEXT);
+CREATE TABLE 'student_personal_details' ('ID' INTEGER,'gender' TEXT,'DOB' TEXT,'ethnicity' TEXT,'academic_info_id' INTEGER,'county_id' INTEGER,'family_details_id' INTEGER);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
